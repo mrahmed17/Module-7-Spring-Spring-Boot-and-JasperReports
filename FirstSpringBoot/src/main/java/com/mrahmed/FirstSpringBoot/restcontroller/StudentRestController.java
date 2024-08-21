0fa7bc714c5200b71758ba60aa58e0d98c7bd128
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-08-21 19:22:04 +0600

    update

---
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/restcontroller/StudentRestController.java
@@ -0,0 +1,45 @@
+package com.mrahmed.FirstSpringBoot.restcontroller;
+
+import com.mrahmed.FirstSpringBoot.entity.Student;
+import com.mrahmed.FirstSpringBoot.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("student/api")
+public class StudentRestController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping("/save")
+    public void saveStudent(@RequestBody Student student) {
+        studentService.saveStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable int id) {
+        studentService.deleteById(id);
+    }
+
+//    @PutMapping("/update/")
+//    public void updateStudent(@RequestBody Student student) {
+//        studentService.saveStudent(student);
+//    }
+
+    @PutMapping("/update/{id}")
+    public void updateStudent(@RequestBody Student student, @PathVariable int id) {
+        studentService.updateStudent(student, id);
+    }
+
+
+
+
+}
--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/StudentService.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/StudentService.java
@@ -30,4 +30,9 @@ public class StudentService {
             return studentRepository.findById(id).get();
         }
 
+
+        public void updateStudent(Student s, int id){
+            studentRepository.save(s);
+        }
+
 }
