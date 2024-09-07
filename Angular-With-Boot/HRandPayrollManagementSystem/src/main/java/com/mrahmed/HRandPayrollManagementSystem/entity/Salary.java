171ae5dc76f6411025c498ad3e57553b09d9e32b
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:36:35 +0600

    Update Salary.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Salary.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Salary.java
@@ -4,11 +4,10 @@ package com.mrahmed.HRandPayrollManagementSystem.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Date;
+import java.sql.Date;
 
 @Entity
 @Table(name = "salaries")
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -17,43 +16,27 @@ public class Salary {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
     private int id;
 
-    @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "employee_id", nullable = false,
-            referencedColumnName = "id")
-    private Employee employee; // basicSalary
+    private int medicare;
+    private boolean overTime; //If yes then add =(baseSalary/4*5)
+    private int fund; // baseSalary * (2% = 0.02)
+    private int netSalary; // (base-advance+bonus+insurance+medicare+overTime-fund)
+    private Date paymentDate;
 
-    @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "advancesalary_id", nullable = false,
-            referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "advancesalary_id")
     private AdvanceSalary advanceSalary; // advancedSalary
-
-
-    @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "bonus_id", nullable = false,
-            referencedColumnName = "id")
-    private Bonus bonus; // bonusAmount
-
-
-    @Column(nullable = false, length=5)
     private int insurance;
 
-    @Column(nullable = false, length=5)
-    private int medicare;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "bonus_id", nullable = false)
+    private Bonus bonus; // bonusAmount
 
-    @Column(nullable = false, length=5)
-    private boolean overTime; //If yes then add =(baseSalary/4*5)
-
-    @Column(nullable = false, length=5)
-    private int fund; // baseSalary * (2% = 0.02)
-
-    @Column(nullable = false)
-    private int netSalary; // (base-advance+bonus+insurance+medicare+overTime-fund)
-
-    @Column(nullable = false)
-    private Date paymentDate;
 
 
 }
