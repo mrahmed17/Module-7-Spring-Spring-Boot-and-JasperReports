518745bde0d66c7bcaf220dcf3f77bcf8e6dd142
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:39:22 +0600

    Update User.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/User.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/User.java
@@ -2,11 +2,10 @@ package com.mrahmed.HRandPayrollManagementSystem.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.util.Date;
+import java.sql.Date;
 
 @Entity
 @Table(name = "users")
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,44 +15,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
-    @Column(length =100, nullable = false)
+    private String userName;
     private String fullName;
-
-    @Column(length =100, nullable = false)
     private String email;
-
-    @Column(length =100, nullable = false)
     private String password;
-
-    @Column(nullable = false)
     private String address;
-
-    @Column(length = 10, nullable = false)
     private String gender;
-
-    @Column(length = 12, nullable = false)
     private Date dateOfBirth;
-
-    @Column(nullable = false, length =16)
     private String nationalId;
-
-    @Column(nullable = false, length = 15)
     private String contact;
-
-    @Column( nullable = false)
     private String role;
-
-    @Column(nullable = true)
     private boolean isActive;
-
-    @Column(nullable = false)
     private Date createdAt;
-
-    @Column(nullable = false)
     private Date updateAt;
-
-    @Column(nullable = false)
     private String profilePhoto;
 
 }
