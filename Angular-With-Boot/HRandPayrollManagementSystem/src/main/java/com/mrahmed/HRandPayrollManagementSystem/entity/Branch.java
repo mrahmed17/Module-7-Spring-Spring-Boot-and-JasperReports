8bc9bfe521275053bc372600c612f326e3dfef6d
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:25:59 +0600

    Update Branch.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Branch.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Branch.java
@@ -5,11 +5,10 @@ package com.mrahmed.HRandPayrollManagementSystem.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Date;
+import java.spl.Date;
 
 @Entity
 @Table(name = "branches")
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,30 +17,14 @@ public class Branch {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = true)
     private int id;
-
-    @Column(nullable = false, length = 100)
     private String name;
-
-    @Column(nullable = false)
     private String address;
-
-    @Column(nullable = false)
     private String city;
-
-    @Column(nullable = false)
     private String zipCode;
-
-    @Column(nullable = false)
     private String country;
-
-    @Column(nullable = false)
     private Date createdAt;
-
-    @Column(nullable = false)
     private Date updateAt;
-
-    @Column(nullable = false)
     private String photo;
+
 }
