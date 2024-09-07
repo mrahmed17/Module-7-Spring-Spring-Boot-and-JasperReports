e050eaafe291ad5c73b38b15b795adf68e5263e6
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:28:42 +0600

    Update Department.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Department.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Department.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 @Entity
 @Table(name = "departments")
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,18 +13,12 @@ public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = true)
     private int id;
-
-    @Column(nullable = false, length = 100)
     private String name;
-
-    @Column(nullable = false, length = 10)
     private String numberOfEmployees;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "branch_id", nullable = false,
-            referencedColumnName = "id")
+    @JoinColumn(name = "branch_id", nullable = false)
     private Branch branch;
 
 
