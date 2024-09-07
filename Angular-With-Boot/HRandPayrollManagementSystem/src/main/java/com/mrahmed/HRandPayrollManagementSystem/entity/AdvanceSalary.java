e230424796905cd369aa8be9fc92fb59d7a19354
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:17:18 +0600

    Update AdvanceSalary.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/AdvanceSalary.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/AdvanceSalary.java
@@ -17,22 +17,14 @@ public class AdvanceSalary {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
     private int id;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "employee_id", nullable = false,
-            referencedColumnName = "id")
+    @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
 
-    @Column(nullable = false, length =5)
     private double advanaceSalary;
-
-
-    @Column(nullable = false)
     private String reason;
-
-    @Column(nullable = false)
     private Date advanceDate;
 
 
