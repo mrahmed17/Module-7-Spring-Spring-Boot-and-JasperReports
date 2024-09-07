40bae8d84b7836b79ad753783332b27ac2062307
Md. Raju Ahmed <tamimahmedraju@gmail.com>
2024-09-07 10:29:14 +0600

    Entity Making

---
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/User.java
@@ -0,0 +1,21 @@
+package com.mrahmed.HRandPayrollManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+}
