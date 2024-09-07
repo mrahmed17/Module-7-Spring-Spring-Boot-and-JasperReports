a7fa1ab01e89c0f567b784d188eecf4dd005c742
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:23:45 +0600

    Update Bonus.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Bonus.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Bonus.java
@@ -8,7 +8,6 @@ import java.util.Date;
 
 @Entity
 @Table(name = "bonuses")
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,20 +17,12 @@ public class Bonus {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
     private int id;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "employee_id", referencedColumnName = "id",
-            nullable = false)
-    private Employee employee;
-
-
-    @Column(nullable = false, length =5)
     private double bonusAmount;
-
-    @Column(nullable = false)
     private Date bonusDate;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
 }
