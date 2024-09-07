5d31b6ad925eacf03c86704c0bf7de50fd8ae244
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-07 20:49:39 +0600

    Update Salary.java

--- Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Salary.java
+++ Angular-With-Boot/HRandPayrollManagementSystem/src/main/java/com/mrahmed/HRandPayrollManagementSystem/entity/Salary.java
@@ -18,23 +18,23 @@ public class Salary {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private int medicare;
+    private Double medicare;
     private boolean overTime; //If yes then add =(baseSalary/4*5)
-    private int fund; // baseSalary * (2% = 0.02)
-    private int netSalary; // (base-advance+bonus+insurance+medicare+overTime-fund)
+    private Double fund; // baseSalary * (2% = 0.02)
+    private Double netSalary; // (base-advance+bonus+insurance+medicare+overTime-fund)
     private Date paymentDate;
+    private Double insurance;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")         
     private User user;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "advancesalary_id")
     private AdvanceSalary advanceSalary; // advancedSalary
-    private int insurance;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "bonus_id", nullable = false)
+    @JoinColumn(name = "bonus_id")
     private Bonus bonus; // bonusAmount
 
 
