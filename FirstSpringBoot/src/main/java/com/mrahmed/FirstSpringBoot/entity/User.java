9e2c5c7295c57bc77322ab968f41a04edebfff91
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:49:41 +0600

    Update User.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/entity/User.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/entity/User.java
@@ -7,20 +7,25 @@ import lombok.NoArgsConstructor;
 
 
 @Entity
-@Table(name = "users")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private  String name;
+
+    private String name;
 
     @Column(unique = true)
     private String email;
     private String password;
     private String image;
+
+
+
 
 }
