827e99bdd52d40bf19e63b611d1d7bbd82a22411
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:52:17 +0600

    Update FacultyRepository.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/FacultyRepository.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/FacultyRepository.java
@@ -2,6 +2,9 @@ package com.mrahmed.FirstSpringBoot.repository;
 
 import com.mrahmed.FirstSpringBoot.entity.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
+
 }
