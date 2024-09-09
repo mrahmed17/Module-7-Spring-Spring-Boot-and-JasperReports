7310b2ad9240fae56de1411cb253e94ae0cc4dfa
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 21:13:39 +0600

    Update Hotel.java

--- ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/entity/Hotel.java
+++ ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/entity/Hotel.java
@@ -7,25 +7,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "hotels")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "hotels")
 public class Hotel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name;
-    private String address;
-    private String  rating;
-    private float minPrice;
-    private float maxPrice;
-    private String image;
+    private  int id;
+    private  String name;
+    private  String address;
+    private  String rating;
+    private  float minPrice;
+    private  float maxPrice;
+    private  String image;
+
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "locationId")
-    private Location location;
+    @JoinColumn(name = "laoctionId")
+    private  Location location;
 
 
 
