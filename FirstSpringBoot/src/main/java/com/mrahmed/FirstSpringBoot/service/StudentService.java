e3d63509d34aee1e10d5f229329193d4f52325df
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 21:04:58 +0600

    Update StudentService.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/StudentService.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/StudentService.java
@@ -5,45 +5,52 @@ import com.mrahmed.FirstSpringBoot.entity.Student;
 import com.mrahmed.FirstSpringBoot.repository.DepartmentRepository;
 import com.mrahmed.FirstSpringBoot.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
-
-public class StudentService {
-
-    @Autowired
-    private StudentRepository studentRepository;
+public class StudentService{
 
     @Autowired
+    private  StudentRepository studentRepository;
+  @Autowired
     private DepartmentRepository departmentRepository;
 
-    public void saveStudent(Student s){
-            Department d=departmentRepository.findById(s.getDepartment().getId())
-                    .orElseThrow(
-                            ()-> new RuntimeException("Student Not Saved ")
-                    );
-            s.setDepartment(d);
+    public  void saveStu(Student s){
 
-            studentRepository.save(s);
-        }
+        Department d=departmentRepository.findById(s.getDepartment().getId())
+                        .orElseThrow(
+                                ()-> new RuntimeException("Student Not Saved ")
+                        );
+        s.setDepartment(d);
+        studentRepository.save(s);
+    }
 
-        public List<Student> getAllStudents(){
-            return studentRepository.findAll();
-        }
+    public  List<Student>  getAllStu(){
 
-        public void deleteById(int id){
-            studentRepository.deleteById(id);
-        }
+         return studentRepository.findAll();
+    }
 
-        public Student findById(int id){
-            return studentRepository.findById(id).get();
-        }
+    public  void deleteById(int id){
+        studentRepository.deleteById(id);
+    }
 
+    public  Student findById(int id){
 
-        public void updateStudent(Student s, int id){
-            studentRepository.save(s);
-        }
+        return  studentRepository.findById(id).get();
+    }
+
+    public  void updateStudent(Student s, int id){
+        studentRepository.save(s);
+
+    }
 
 }
