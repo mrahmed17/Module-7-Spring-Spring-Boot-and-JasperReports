8d2b44bc5dd561e2d5f2cb092cd6cec3beb2006c
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 21:22:08 +0600

    Update LocationService.java

--- ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/service/LocationService.java
+++ ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/service/LocationService.java
@@ -9,27 +9,27 @@ import java.util.List;
 @Service
 public class LocationService {
 
-    private LocationRepository locationRepository;
+     private LocationRepository locationRepository;
 
-    public List<Location> getALlLocation(){
+     public List<Location> getALlLocation(){
 
-        return locationRepository.findAll();
+         return locationRepository.findAll();
 
-    }
+     }
 
-    public  void saveLocation(Location l){
-        locationRepository
-                .save(l);
-    }
+     public  void saveLocation(Location l){
+         locationRepository
+                 .save(l);
+     }
 
-    public  void deleteLocation(int id){
-        locationRepository.deleteById(id);
-    }
+     public  void deleteLocation(int id){
+         locationRepository.deleteById(id);
+     }
 
-    public  Location findById(int id){
-        return   locationRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Location Not Foound by this Id"));
-    }
+     public  Location findById(int id){
+       return   locationRepository.findById(id)
+               .orElseThrow(()-> new RuntimeException("Location Not Foound by this Id"));
+     }
 
 
 
