265639b9ee77f5bbdb747868f1b51f90531d1b16
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:38:35 +0600

    Update HomeController.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/HomeController.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/HomeController.java
@@ -1,19 +1,20 @@
-package com.mrahmed.FirstSpringBoot.controller;
+package com.emranhss.FirstSpringBoot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(){
+    public  String home(){
+
         return "index";
     }
 
     @RequestMapping("/contact")
-    public String contact(){
+    public String contact() {
+
         return "contact";
     }
 
