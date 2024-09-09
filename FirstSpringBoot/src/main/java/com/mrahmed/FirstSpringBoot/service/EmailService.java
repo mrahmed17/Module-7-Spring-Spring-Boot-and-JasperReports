32adc1d0c8b0e995f6d943d685039580d9ac873e
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 21:01:58 +0600

    Update EmailService.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/EmailService.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/service/EmailService.java
@@ -1,11 +1,10 @@
 package com.mrahmed.FirstSpringBoot.service;
 
-
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,12 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+
+
         javaMailSender.send(mes);
     }
+
+
+
 
 }
