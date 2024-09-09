54efc06fda334b3565f8ca504c5c1f6e87615fd1
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:42:14 +0600

    Update StudentController.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/StudentController.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/StudentController.java
@@ -2,11 +2,13 @@ package com.mrahmed.FirstSpringBoot.controller;
 
 import com.mrahmed.FirstSpringBoot.entity.Student;
 import com.mrahmed.FirstSpringBoot.service.StudentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,39 +19,44 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+
     @RequestMapping("/savestudentform")
-    public String saveStudentForm(Model m) {
+    public String saveStudent(Model m) {
         m.addAttribute("student", new Student());
         m.addAttribute("title", "Add New Student");
         return "savestudentform";
     }
 
-    @RequestMapping(value = "/saveStudent")
+    @PostMapping("/savestudent")
     public String saveStudent(@ModelAttribute("student") Student student) {
-       studentService.saveStudent(student);
-       return "redirect:/showAllStudent";
+        studentService.saveStu(student);
+        return "redirect:/showAllStudent";
     }
 
     @RequestMapping("/showAllStudent")
     public String showAllStudent(Model m) {
-        List<Student> studentList = studentService.getAllStudents();
-        m.addAttribute("title", "Al Students");
-        m.addAttribute("studentList", studentList);
+        List<Student> stuList = studentService.getAllStu();
+        m.addAttribute("stuList", stuList);
+        m.addAttribute("title", "All Student");
+
         return "showAllStudent";
     }
 
-    @RequestMapping(value = "/deleteStudent/{id}")
-    public String deleteStudent(@PathVariable ("id") int id) {
+    @RequestMapping("deletestudent/{id}")
+    public String deleteStudent(@PathVariable("id") int id) {
         studentService.deleteById(id);
         return "redirect:/showAllStudent";
     }
 
     @RequestMapping("/editstudent/{id}")
-    public String  editStudent(@PathVariable ("id") int id, Model m) {
-        m.addAttribute("title", "Edit Student");
-        Student student =studentService.findById(id);
-        m.addAttribute("student", student);
+    public  String editStudent(@PathVariable("id") int id, Model m){
+        Student s=studentService.findById(id);
+
+        m.addAttribute("student", s);
+
         return "savestudentform";
+
     }
+
 
 }
