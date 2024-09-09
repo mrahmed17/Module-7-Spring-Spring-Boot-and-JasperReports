04c257b0ce61d159eb5927e62841d8a6059cec61
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:52:54 +0600

    Update StudentRepository.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/StudentRepository.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/StudentRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-
 
 
 }
