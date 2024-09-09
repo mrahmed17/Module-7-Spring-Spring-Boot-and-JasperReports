ba09463baf17f0c43b7936e1cfaea8db4cc363e4
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:40:24 +0600

    Update HomeController.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/HomeController.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.emranhss.FirstSpringBoot.controller;
+package com.mrahmed.FirstSpringBoot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
