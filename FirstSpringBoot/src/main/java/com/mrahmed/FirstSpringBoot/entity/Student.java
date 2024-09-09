8cbe70076f7d6d1d108e12ae1a8e2ee7199931b9
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:48:59 +0600

    Update Student.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/entity/Student.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/entity/Student.java
@@ -10,29 +10,28 @@ import lombok.NoArgsConstructor;
 import java.sql.Date;
 
 @Entity
-@Table(name="students")
+@Table(name = "students")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @Column(nullable = false, length = 40, name = "studnetName")
+    private  int id;
+    @Column(nullable = false, length = 40, name = "studentName")
     private String name;
     @Column(nullable = false, unique = true)
-    private String email;
+    private  String email;
+
     @Column(nullable = false, unique = true)
-    private String cell;
-    private String gender;
+    private  String cell;
+    private  String gender;
 
-    @Temporal(TemporalType.DATE)
-    private Date dob; //java.sql.Date
-
+    private Date dob;
     @ManyToOne
     @JoinColumn(name = "departmentId")
     private Department department;
-
 
 
 }
