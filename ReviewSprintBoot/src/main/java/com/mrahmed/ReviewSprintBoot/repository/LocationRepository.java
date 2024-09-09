917d458163bef79c89bb56de39bffc00fe6844bc
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 21:18:17 +0600

    Update LocationRepository.java

--- ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/repository/LocationRepository.java
+++ ReviewSprintBoot/src/main/java/com/mrahmed/ReviewSprintBoot/repository/LocationRepository.java
@@ -4,6 +4,10 @@ import com.mrahmed.ReviewSprintBoot.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
-public interface LocationRepository extends JpaRepository <Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+
 }
