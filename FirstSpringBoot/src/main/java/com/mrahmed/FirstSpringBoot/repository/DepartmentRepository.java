c97c4e1b7041f849dfde66b1b50ddaba678a5d29
Md. Raju Ahmed <138282465+mrahmed17@users.noreply.github.com>
2024-09-09 20:51:17 +0600

    Update DepartmentRepository.java

--- FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/DepartmentRepository.java
+++ FirstSpringBoot/src/main/java/com/mrahmed/FirstSpringBoot/repository/DepartmentRepository.java
@@ -2,8 +2,8 @@ package com.mrahmed.FirstSpringBoot.repository;
 
 import com.mrahmed.FirstSpringBoot.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DepartmentRepository extends JpaRepository
-        <Department, Integer> {
-
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 }
