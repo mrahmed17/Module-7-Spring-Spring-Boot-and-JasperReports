769fbaaabbdba8f9352c83f7b1aee144cf35bcb9
Md. Raju Ahmed <tamimahmedraju@gmail.com>
2024-09-25 11:42:31 +0600

    Update

--- Angular-With-Boot/Running Boot/src/main/java/com/mrahmed/HRandPayrollManagementSystem/webconfig/WebConfig.java
+++ Angular-With-Boot/Running Boot/src/main/java/com/mrahmed/HRandPayrollManagementSystem/webconfig/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
                         .allowedOrigins("http://localhost:4200","http://127.0.0.1:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
-//                        .allowCredentials(true);
+                    //  .allowCredentials(true);
             }
         };
     }
