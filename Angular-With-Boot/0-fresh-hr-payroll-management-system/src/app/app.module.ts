04ac834e10fea8a4063f9622af930973281c4179
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-10-05 19:08:26 +0600

    Template

--- Angular-With-Boot/0-fresh-hr-payroll-management-system/src/app/app.module.ts
+++ Angular-With-Boot/0-fresh-hr-payroll-management-system/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './template/navbar/navbar.component';
 import { SidebarComponent } from './template/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
